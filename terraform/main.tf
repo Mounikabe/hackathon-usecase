994573e55516ad171318cf8cd56ee91496a96f56
Mounikabe <128820491+Mounikabe@users.noreply.github.com>
2025-11-08 16:43:20 +0530

    updated credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "google" {
-  project = "peppy-plateau-477609-c8"
-  region  = "us-central1"
-  credentials = secrets.GCP_SA_KEY
+  credentials = env("GCP_SA_KEY")  
+  project     = "peppy-plateau-477609-c8"
+  region      = "us-central1"
 }
 
 resource "google_compute_network" "main_vpc" {
@@ -18,7 +18,6 @@ resource "google_compute_network" "main_vpc" {
   auto_create_subnetworks = false
 }
 
-# Public Subnets (2 AZs)
 resource "google_compute_subnetwork" "public_subnet_a" {
   name          = "public-subnet-a"
   ip_cidr_range = "10.10.1.0/24"
@@ -33,32 +32,33 @@ resource "google_compute_subnetwork" "public_subnet_b" {
   network       = google_compute_network.main_vpc.id
 }
 
-# Private Subnets (2 AZs)
 resource "google_compute_subnetwork" "private_subnet_a" {
-  name          = "private-subnet-a"
-  ip_cidr_range = "10.10.3.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.main_vpc.id
+  name                     = "private-subnet-a"
+  ip_cidr_range            = "10.10.3.0/24"
+  region                   = "us-central1"
+  network                  = google_compute_network.main_vpc.id
   private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "private_subnet_b" {
-  name          = "private-subnet-b"
-  ip_cidr_range = "10.10.4.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.main_vpc.id
+  name                     = "private-subnet-b"
+  ip_cidr_range            = "10.10.4.0/24"
+  region                   = "us-central1"
+  network                  = google_compute_network.main_vpc.id
   private_ip_google_access = true
 }
 
-##storage account##
+# Storage bucket for Terraform state
 resource "google_storage_bucket" "terraform_state" {
   name                        = "tfstate-demo-bucket-12345"
   location                    = "us-central1"
   force_destroy               = true
   uniform_bucket_level_access = true
+
   versioning {
     enabled = true
   }
+
   lifecycle_rule {
     action {
       type = "Delete"
